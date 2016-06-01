47644deeffdf95766b42bd1118c3fb3b01f6e3a9
Devin Pastoor <devin.pastoor@gmail.com>
2016-06-01 14:41:14 +0200

    more tests for baby

--- test/test-baby.js
+++ test/test-baby.js
@@ -3,14 +3,19 @@
  * Created by devin on 6/1/16.
  */
 import test from 'ava';
-import {Baby} from '../src/stores/baby'
+import {Baby} from '../src/stores/baby';
+import {toJS} from 'mobx';
 test(t => {
   const baby1 = new Baby("Dan", "2016", 2.5, 0.9, "Michel");
   t.true(baby1.attendingDoctor === "Michel")
+  t.deepEqual(
+    toJS(baby1.serumCreatinineRecords),
+    [{time:"2016", serumCreatinine: 0.9}]
+  )
 });
 
 test(t => {
   const baby2 = new Baby("Dan", "2016", 2.5, 0.9, "Michel");
-  baby2.addSerumCreatinine("2016", 0.9)
+  baby2.addSerumCreatinine("2016", 0.9);
   t.true(baby2.serumCreatinineRecords[0].serumCreatinine === 0.9)
 });
