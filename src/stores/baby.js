6eaf2ca6daee9369ea482abde0c21a27f7119415
Devin Pastoor <devin.pastoor@gmail.com>
2016-06-01 13:34:18 +0200

    add basic baby class implementation

---
+++ src/stores/baby.js
@@ -0,0 +1,57 @@
+import { observable, computed, transaction } from 'mobx'
+
+
+export class Baby {
+  @observable weightRecords;
+  @observable serumCreatinineRecords;
+  @observable concentrationRecords;
+  @observable dosingRecords;
+  constructor(name, timeOfBirth, weight, serumCreatinine) {
+    this.name = name;
+    this.timeOfBirth = timeOfBirth;
+    this.weightRecords = [];
+    this.serumCreatinineRecords = [];
+    this.concentrationRecords = [];
+    this.dosingRecords = [];
+    this.addWeight(timeOfBirth, weight);
+    this.addSerumCreatinine( timeOfBirth, serumCreatinine);
+  }
+  addWeight(time, weight){
+    this.weightRecords.push(new WeightRecord(time, weight))
+  }
+  addSerumCreatinine(time, serumCreatinine) {
+    this.serumCreatinineRecords.push(new SerumCreatinineRecord(time, weight))
+  }
+  addConcentrationRecord(time, concentration) {
+    this.concentrationRecords.push(new ConcentrationRecord(time, concentration))
+  }
+  addDosingRecord(time, dose) {
+    this.dosingRecords.push(new DosingRecord(time, dose))
+  }
+}
+
+export class WeightRecord {
+  constructor(time, weight) {
+    this.time = time;
+    this.weight = weight;
+  }
+}
+export class SerumCreatinineRecord {
+  constructor(time, serumCreatinine) {
+    this.time = time;
+    this.serumCreatinine = serumCreatinine;
+  }
+}
+
+export class ConcentrationRecord {
+  constructor(time, concentration) {
+    this.time = time;
+    this.concentration = concentration;
+  }
+}
+export class DosingRecord {
+  constructor(time, dosing) {
+    this.time = time;
+    this.dosing = dosing;
+  }
+}
