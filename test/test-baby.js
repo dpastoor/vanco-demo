f8747f49c123c24ad9ef44b1d1269622eb8c9571
Devin Pastoor <devin.pastoor@gmail.com>
2016-06-01 14:33:28 +0200

    add some basic baby tests

--- test/test-baby.js
+++ test/test-baby.js
@@ -3,8 +3,14 @@
  * Created by devin on 6/1/16.
  */
 import test from 'ava';
+import {Baby} from '../src/stores/baby'
+test(t => {
+  const baby1 = new Baby("Dan", "2016", 2.5, 0.9, "Michel");
+  t.true(baby1.attendingDoctor === "Michel")
+});
 
 test(t => {
-  const a = 1;
-  t.true(a === 0)
+  const baby2 = new Baby("Dan", "2016", 2.5, 0.9, "Michel");
+  baby2.addSerumCreatinine("2016", 0.9)
+  t.true(baby2.serumCreatinineRecords[0].serumCreatinine === 0.9)
 });
